018c65412f1f35c2742f7fbfacb8e54f8e14872c
norm02 <richa0920@gmail.com>
2023-05-17 09:52:53 +0900

    Cypressコード更新

--- cypress/e2e/SignIn_spec.cy.js
+++ cypress/e2e/SignIn_spec.cy.js
@@ -7,14 +7,15 @@ describe("react_app_auth_test", () => {
   it("ログイン成功パターン", () => {
     cy.visit("http://localhost:3000/");
     cy.get('input[name="email"]').type(input.email);
-    //cy.get('input[name="password"]').type(input.password);
+    cy.get('input[name="password"]').type(input.password);
     cy.get(".signin-button").click();
     cy.url().should("eq", "http://localhost:3000/");
   });
   it("ログイン失敗パターン", () => {
     cy.visit("http://localhost:3000/");
-    cy.get('input[name="email"]').type("myemail@user.com");
+    cy.get('input[name="email"]').type(input.email);
+    //cy.get('input[name="password"]').type(input.password);
     cy.get(".signin-button").click();
-    cy.contains("パスワードを入力してください。");
+    cy.url().should("eq", "http://localhost:3000/");
   });
 });
